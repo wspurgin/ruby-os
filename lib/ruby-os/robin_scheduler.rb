4244338a4b19911cb7cd4eaa4707c98c6b5fca0b
Will Spurgin <will.spurgin@gmail.com>
2016-03-29 20:13:17 -0500

    Add and Spec out RobinScheduler

---
+++ lib/ruby-os/robin_scheduler.rb
@@ -0,0 +1,36 @@
+require 'ruby-os/scheduler'
+
+class RubyOS::RobinScheduler < RubyOS::Scheduler
+  # This scheduler maintains it own "clock" so to say, to manage the share each
+  # process has in the CPU. It does not sorting. It's basic FIFO but with a
+  # round robin sharing based on the `q` parameter.
+
+  attr_reader :q, :timer
+
+  def initialize(queue_manager, q)
+    super(queue_manager)
+    raise ArgumentError.new "Expected 2nd param to be a Numeric" if !q.is_a?(Numeric)
+    @q = q.to_int
+    @timer = @q
+  end
+
+  def next_proc(queue_identifier, current_proc=nil)
+    super
+
+    return queue_manager[queue_identifier].pop if current_proc.nil? && reset!
+
+    # The current proc still has time left.
+    if (@timer -= 1) == 0
+      # Time share is up, reset timer and return next process
+      reset!
+      queue_manager[queue_identifier].pop
+    else
+      current_proc
+    end
+  end
+
+  def reset!
+    @timer = q
+  end
+
+end
---
+++ spec/robin_scheduler_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe RubyOS::RobinScheduler do
+  describe "#next_proc" do
+    before do
+      @manager = RubyOS::QueueManager.new
+      @manager.add_queue(:ready)
+      [1,2,3,4,5,6].each do |pid|
+        @manager[:ready].push RubyOS::PCB.new pid, 0x2
+      end
+      @q = 2
+      @scheduler = described_class.new @manager, @q
+    end
+
+    it "should return the current_proc if the time share is not over" do
+      current_proc = RubyOS::PCB.new 2931, 0x2
+      expect(@scheduler.next_proc :ready, current_proc).to eq current_proc
+    end
+
+    context "there is no current_proc" do
+      it "should return the next proc in the queue and reset the timer" do
+        expected_proc =  @manager[:ready].first
+        expect(@scheduler.next_proc :ready).to eq expected_proc
+        expect(@scheduler.timer).to eq @q
+      end
+    end
+
+    context "the current_proc is still in the CPU and the timer share runs out" do
+      it "should return the next proc and reset the timer" do
+        current_proc = RubyOS::PCB.new 2931, 0x2
+        expected_proc =  @manager[:ready].first
+
+        # run the scheduler once to get the run the timer down
+        @scheduler.next_proc :ready, current_proc
+        expect(@scheduler.next_proc :ready, current_proc).to eq expected_proc
+      end
+    end
+  end
+end

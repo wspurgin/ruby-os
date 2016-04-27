88b00f08277861cc6efa78e98a971f5fb4447dd1
Will Spurgin <will.spurgin@gmail.com>
2016-04-27 17:12:35 -0500

    Refactor to use `find_hole` base class method

--- lib/ruby-os/memory/first_fit_manager.rb
+++ lib/ruby-os/memory/first_fit_manager.rb
@@ -3,40 +3,23 @@ module RubyOS::Memory
     def assign_process_base_address(process)
       super
       # Find the first available address
-      starting_address, _ = memory_map.find(proc { [nil, nil] }) { |addr, flag| flag == FREE }
-      raise OutOfMemoryError.new if starting_address.nil?
+      hole = find_hole
+      raise OutOfMemoryError.new if hole.nil?
 
-      # Find the first reserved location
-      first_reserved, _ = memory_map
-        .find(proc { [nil, nil] }) { |addr, flag| addr > starting_address and flag == RESERVED }
-
-      # if the first_reserved address is nil, then there is only one available
-      # space after the starting address, so the 'first_reserved' address is
-      # the highest_address + 1 (which doesn't technically exist, but we can't
-      # go past the total available space so it's basically reserved...).
-      first_reserved = highest_address + 1 if first_reserved.nil?
-
-      if first_reserved - starting_address >= process.memory_required
-        process.base_address = starting_address
+      if hole[:mem_available] >= process.memory_required
+        process.base_address = hole[:base_address]
         process.memory_limit = process.memory_required
       else
         loop do
           # The space between starting_address and first_reserved is not enough
           # for the requesting process. So continue on to the next 'hole'
-          starting_address, _ = memory_map
-            .find(proc { [nil, nil] }) { |addr, flag| addr > first_reserved and flag == FREE }
+          hole = find_hole(hole[:end_address])
 
           # if the address is nil, then there were no further 'holes'
-          raise NoContiguousMemoryError.new if starting_address.nil?
+          raise NoContiguousMemoryError.new if hole.nil?
 
-          first_reserved, _ = memory_map
-            .find(proc { [nil, nil] }) { |addr, flag| addr > starting_address and flag == RESERVED }
-
-          # If it's nil, the highest_address is the last free spot
-          first_reserved = highest_address + 1 if first_reserved.nil?
-
-          if first_reserved - starting_address >= process.memory_required
-            process.base_address = starting_address
+          if hole[:mem_available] >= process.memory_required
+            process.base_address = hole[:base_address]
             process.memory_limit = process.memory_required
             break
           end
@@ -44,7 +27,6 @@ module RubyOS::Memory
       end
 
       reserve_memory(process.base_address, process.memory_limit)
-      # TODO rescue OutOfBoundsAssignment error here or let the OS handled it?
     end
   end
 end
